f5a2c749760ec6a2bd0c49a558dc27a8bd6992c0
Babu Chellathurai <babu.abps@gmail.com>
2021-03-16 00:25:31 -0400

    Auto-generated commit

---
+++ webpush-notifications-app/service-worker.js
@@ -0,0 +1,47 @@
+importScripts('./ngsw-worker.js');
+
+self.addEventListener('fetch', function (event) { });
+
+self.addEventListener('notificationclick', function (e) {
+    var notification = e.notification;
+    var action = e.action;
+
+    if (action === 'close') {
+        notification.close();
+    } else {
+        clients.openWindow('http://www.google.com');
+        notification.close();
+    }
+});
+
+self.addEventListener('push', function (e) {
+    var body;
+
+    if (e.data) {
+        body = JSON.parse(e.data.text());
+    } else {
+        body = "Standard Message";
+    }
+
+    var options = {
+        body: body.Message,
+        icon: "assets/icons/icon-512x512.png",
+        vibrate: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now()
+        },
+        actions: [
+            {
+                action: "explore", title: "Go interact with this!",
+                icon: "assets/icons/checkmark.png"
+            },
+            {
+                action: "close", title: "Ignore",
+                icon: "assets/icons/red_x.png"
+            },
+        ]
+    };
+    e.waitUntil(
+        self.registration.showNotification(body.Title, options)
+    );
+});
